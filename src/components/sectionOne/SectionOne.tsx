3aaf5a40a38e1b718904e87023e8975638d84629
Elijah Adeyeye <elijahadeyeye@Elijahs-MacBook-Air.local>
2025-10-15 17:50:32 +0100

    logo_height

--- src/components/sectionOne/SectionOne.tsx
+++ src/components/sectionOne/SectionOne.tsx
@@ -4,35 +4,35 @@ import Marquee from "react-fast-marquee";
 
 function SectionOne() {
   return (
-    <Marquee className="bg-white">
-  <div className="flex items-center text-white font-extrabold font-sans py-5 gap-[50px] md:gap-[80px] lg:gap-[150px]">
-    <img
-      className="h-[70px] w-auto md:h-[100px] lg:h-[120px]"
-      src="/sotab_logo.png"
-      alt="logo"
-    />
-    <img
-      className="h-[70px] w-auto md:h-[100px] lg:h-[120px]"
-      src="/sotab_logo.png"
-      alt="logo"
-    />
-    <img
-      className="h-[70px] w-auto md:h-[100px] lg:h-[120px]"
-      src="/sotab_logo.png"
-      alt="logo"
-    />
-    <img
-      className="h-[70px] w-auto md:h-[100px] lg:h-[120px]"
-      src="/sotab_logo.png"
-      alt="logo"
-    />
-    <img
-      className="h-[70px] w-auto md:h-[100px] lg:h-[120px]"
-      src="/sotab_logo.png"
-      alt="logo"
-    />
-  </div>
-</Marquee>
+    <Marquee className="bg-white py-8">
+    <div className="flex items-center gap-[80px] md:gap-[100px] lg:gap-[150px]">
+      <img
+        className="h-[80px] w-auto object-contain md:h-[100px] lg:h-[120px]"
+        src="/sotab_logo.png"
+        alt="logo"
+      />
+      <img
+        className="h-[80px] w-auto object-contain md:h-[100px] lg:h-[120px]"
+        src="/sotab_logo.png"
+        alt="logo"
+      />
+      <img
+        className="h-[80px] w-auto object-contain md:h-[100px] lg:h-[120px]"
+        src="/sotab_logo.png"
+        alt="logo"
+      />
+      <img
+        className="h-[80px] w-auto object-contain md:h-[100px] lg:h-[120px]"
+        src="/sotab_logo.png"
+        alt="logo"
+      />
+      <img
+        className="h-[80px] w-auto object-contain md:h-[100px] lg:h-[120px]"
+        src="/sotab_logo.png"
+        alt="logo"
+      />
+    </div>
+  </Marquee>
   );
 }
 
