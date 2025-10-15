084a926cde81acf215bc5b50a31e1d7148e81461
Elijah Adeyeye <elijahadeyeye@Elijahs-MacBook-Air.local>
2025-10-15 17:24:41 +0100

    correction

--- src/components/sectionOne/SectionOne.tsx
+++ src/components/sectionOne/SectionOne.tsx
@@ -5,25 +5,34 @@ import Marquee from "react-fast-marquee";
 function SectionOne() {
   return (
     <Marquee className="bg-white">
-      <div className="flex  text-white font-extrabold font-sans py-5 gap-[5%] md:gap-[1%] lg:gap-[20%]">
-        {" "}
-        <img
-          className="w-[100%] h-[70px] md:w-[45%] md:h-[100px] lg:h-[120px]"
-          src="/sotab_logo.png"
-          alt="logo"
-        />
-        <img
-          className="w-[100%] h-[70px] md:w-[45%] md:h-[100px] lg:h-[120px]"
-          src="/sotab_logo.png"
-          alt="logo"
-        />
-        <img
-          className="w-[100%] h-[70px] md:w-[45%] md:h-[100px] lg:h-[120px] hidden md:block"
-          src="/sotab_logo.png"
-          alt="logo"
-        />
-      </div>
-    </Marquee>
+  <div className="flex items-center text-white font-extrabold font-sans py-5 gap-[50px] md:gap-[80px] lg:gap-[150px]">
+    <img
+      className="h-[70px] w-auto md:h-[100px] lg:h-[120px]"
+      src="/sotab_logo.png"
+      alt="logo"
+    />
+    <img
+      className="h-[70px] w-auto md:h-[100px] lg:h-[120px]"
+      src="/sotab_logo.png"
+      alt="logo"
+    />
+    <img
+      className="h-[70px] w-auto md:h-[100px] lg:h-[120px]"
+      src="/sotab_logo.png"
+      alt="logo"
+    />
+    <img
+      className="h-[70px] w-auto md:h-[100px] lg:h-[120px]"
+      src="/sotab_logo.png"
+      alt="logo"
+    />
+    <img
+      className="h-[70px] w-auto md:h-[100px] lg:h-[120px]"
+      src="/sotab_logo.png"
+      alt="logo"
+    />
+  </div>
+</Marquee>
   );
 }
 
